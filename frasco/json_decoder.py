e93fe827692bbec2f1916ac1e8f3f3229d8f8e3a
Maxime Bouroumeau-Fuseau <maxime.bouroumeau@gmail.com>
2020-07-09 11:03:45 +0200

    [frasco] Handle map type in json_decoder

--- frasco/json_decoder.py
+++ frasco/json_decoder.py
@@ -13,7 +13,7 @@ class JSONEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, speaklater._LazyString):
             return o.value
-        if isinstance(o, set):
+        if isinstance(o, (set, map)):
             return list(o)
         if isinstance(o, datetime.time):
             return str(o)
