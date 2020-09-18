5d1a8df2eb89e355015562d7df855b9e0624886a
Maxime Bouroumeau-Fuseau <maxime.bouroumeau@gmail.com>
2020-09-18 18:04:42 +0200

    [angular] Fix scanning of dir to find templates in build_templates() (os.scandir is not recursive)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='frasco',
-    version='2.0.0',
+    version='2.0.1',
     url='http://github.com/frascoweb/frasco',
     license='MIT',
     author='Maxime Bouroumeau-Fuseau',
