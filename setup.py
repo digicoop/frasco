a737c4f437b1ae515a0d2c6725a9f93151dac9ae
Maxime Bouroumeau-Fuseau <maxime.bouroumeau@gmail.com>
2021-01-06 15:02:18 +0100

    updated version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='frasco',
-    version='2.2.3',
+    version='2.2.4',
     url='http://github.com/frascoweb/frasco',
     license='MIT',
     author='Maxime Bouroumeau-Fuseau',
