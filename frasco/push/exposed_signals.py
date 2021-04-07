6f7dc7976afaeac6d39555432ca091951a613756
Maxime Bouroumeau-Fuseau <maxime.bouroumeau@gmail.com>
2021-04-07 12:00:01 +0200

    [push] Better room_getter for exposed_signal()

--- frasco/push/exposed_signals.py
+++ frasco/push/exposed_signals.py
@@ -7,22 +7,25 @@ exposed_signal_emitted = signal('exposed_signal_emitted')
 
 
 def expose_signal(signal, marshaller=None, room_getter=None, push_event_name=None, **kwargs):
+    if not push_event_name:
+        push_event_name = signal.name
+
     def listener(sender, **kw):
+        emit_kwargs = dict(kwargs)
+        if '_push_skip_self' in kw:
+            emit_kwargs['skip_self'] = kw.pop('_push_skip_self')
         if marshaller:
             data = marshaller(sender, **kw)
         else:
             data = kw or None
         if room_getter:
-            room = room_getter(sender)
+            room, is_user = room_getter(sender)
         else:
-            room = getattr(sender, 'exposed_signal_room', None)
-        emit_kwargs = dict(kwargs)
-        if '_push_skip_self' in kw:
-            emit_kwargs['skip_self'] = kw.pop('_push_skip_self')
-        if room:
-            emit_push_event(push_event_name or signal.name, data, room=room, **emit_kwargs)
+            room, is_user = getattr(sender, 'exposed_signal_room', (None, False))
+        if is_user:
+            emit_user_push_event(room, push_event_name, data, **emit_kwargs)
         else:
-            emit_user_push_event(push_event_name or signal.name, data, **emit_kwargs)
+            emit_push_event(push_event_name, data, room=room, **emit_kwargs)
         exposed_signal_emitted.send(sender, signal_name=signal.name, signal_data=kw, push_data=data)
 
     listener.marshaller = marshaller
--- frasco/tasks/helpers.py
+++ frasco/tasks/helpers.py
@@ -4,8 +4,8 @@ from . import enqueue_task
 def connect_as_background_task(signal, **kwargs):
     def decorator(func):
         kwargs['weak'] = False
-        @signal.connect(**kwargs)
         def listener(sender, **kwargs):
             enqueue_task(func, sender, **kwargs)
+        signal.connect(listener, **kwargs)
         return func
     return decorator
